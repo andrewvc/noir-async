86ff6964ed86cd7523aeeee31e8d140187315a50
Andrew Cholakian <andrew@andrewvc.com>
2012-01-24 22:50:10 -0800

    Bump to 0.1.2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject noir-async "0.1.1-SNAPSHOT"
+(defproject noir-async "0.1.2"
   :description "Async support for noir. Based on Aleph"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-alpha4"]
-                 [aleph "0.2.1-SNAPSHOT"] ]
+                 [aleph "0.2.1-alpha1"] ]
   :dev-dependencies [[midje "1.3.1"]
                      [lein-midje "1.0.7"]
                      [lein-clojars "0.7.0"]])
