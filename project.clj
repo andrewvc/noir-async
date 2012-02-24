08b1727440bcbdd1f832f05d5d4648a975d197ca
Andrew Cholakian <andrew@andrewvc.com>
2012-02-23 21:26:07 -0800

    Upgrade to latest aleph w/ firefox websocket support

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject noir-async "0.1.3"
+(defproject noir-async "0.1.4"
   :description "Async support for noir. Based on Aleph"
+  :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-alpha4"]
-                 [aleph "0.2.1-alpha2-SNAPSHOT"] ]
+                 [aleph "0.2.1-alpha2-20120222.024435-6"]]
   :dev-dependencies [[midje "1.3.1"]
                      [lein-midje "1.0.7"]
                      [lein-clojars "0.7.0"]])
