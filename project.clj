2008e5818d4c369b82b121d3960b47a96fde1b29
Andrew Cholakian <andrew@andrewvc.com>
2012-03-24 19:53:00 -0700

    Partial refactor work

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-
-(defproject noir-async "0.1.4"
+(defproject noir-async "1.0.0-ALPHA1"
   :description "Async support for noir. Based on Aleph"
   ;:warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir "1.3.0-alpha4"]
+                 [noir "1.3.0-beta2"]
                  [aleph "0.2.1-beta1"]]
   :dev-dependencies [[midje "1.3.1"]
                      [lein-midje "1.0.7"]
--- src/noir_async/core.clj
+++ src/noir_async/core.clj
@@ -1,71 +1,77 @@
 (ns noir-async.core
-  (:require [lamina.core :as l])
-  (:use aleph.http
-        noir.core))
+  (require [lamina.core :as lc]
+           [aleph.http :as ah]
+           [noir.core :as nc]))
 
-(defprotocol Closable
-  "Represents a closable connection"
-  (close [this]))
+(defn- format-response
+  "Format responses for a one shot response"
+  [response]
+  (cond (string? response) {:status 200 :body response}
+        :else              response))
 
-(defprotocol MessageStreamPattern
-  "Protocol for a bidirectional message-based stream"
-  (send-message [this message])
-  (on-receive [this handler])
-  (on-close   [this handler]))
+(defn- downstream-ch
+  [{:keys [request-channel response-channel]}]
+  "Returns the downstream channel for a connection"
+  (or response-channel request-channel))
 
-(defrecord WebSocketConnection [request-channel ring-request]
-  MessageStreamPattern
-  (send-message [this message] 
-    (l/enqueue request-channel message))
-  (on-receive [this handler]
-    (l/receive-all request-channel handler))
-  (on-close [this handler]
-    (l/on-closed request-channel handler))
-  Closable
-  (close [this]
-    (l/close request-channel)))
+(defn websocket?
+  "Is the connection opened from the client as a websocket?"
+  [{r :ring-request}]
+  (boolean (:websocket r)))
 
-(defn websocket-connection [request-channel ring-request]
-  (WebSocketConnection. request-channel ring-request))
+(defn close
+  "Closes the connection. No more data can be sent / received after this"
+  [{:keys [request-channel response-channel]}]
+  (lc/close request-channel)
+  (when response-channel
+    (lc/close response-channel)))
 
-(defprotocol RequestReplyPattern
-  "A protocol suitable for an asynchronous connection
-   that will receive a single response"
-  (respond [this response]))
+(defn resp
+  "Respond to a request in one shot"
+  [conn response]
+  (if (and (not (websocket? conn)) 
+    (lc/enqueue-and-close (:request-channel conn) (format-response response))
+    (throw (Exception. "Attempted to call 'resp' on a websocket or chunked resp."))))
 
-(defrecord PageConnection
+(defn- valid-header-map?
+  [header-map]
+  (and
+   (map? header-map)
+   (not (:body header-map))))
+
+(defn resp-part
+  "Sends a message across a websocket or chunked connection"
+  [{:keys [request-channel response-channel state]} msg]
+  (if (websocket? conn)
+    (lc/enqueue request-channel msg)
+    (do
+      (cond
+       (not (compare-and-set! state :initialized :header-sent))
+         (lc/enqueue 
+       :else
+         (do 
+           (compare-and-set! response-channel nil (channel))
+           (if (valid-header-map? msg)
+             (lc/enqueue request-channel (assoc msg :body @response-channel))
+             (throw (Exception. "First message should be a valid header map")))
+
+(defn on-close
+  "Callback to invoke on connection close."
+  [{:keys [request-channel]} handler]
+  (lc/on-closed (request-channel) handler))
+
+(defn on-receive
+  "Callback to invoke on receipt of a websocket message."
+  [{:keys [request-channel]} handler]
+  (lc/receive-all request-channel handler))
+
+(defn- connection
   [request-channel ring-request]
-  RequestReplyPattern
-  (respond [this response]
-    (l/enqueue-and-close request-channel
-        (cond (string? response) {:status 200 :body response}
-              :else              response)))
-  Closable
-  (close [this]
-    (l/close request-channel)))
-
-(defn page-connection [request-channel ring-request]
-  (PageConnection. request-channel ring-request))
-
-(defprotocol ChunkedReplyPattern
-  "A protocol suitable for a chunked reply"
-  (send-header [this headers])
-  (send-chunk [this chunk])
-  (send-last-chunk [this chunk]))
-
-(defrecord ChunkedConnection [request-channel ring-request response-channel]
-  ChunkedReplyPattern
-  (send-header [this headers]
-    (l/enqueue response-channel (assoc headers :body response-channel)))
-  (send-chunk [this chunk]
-    (l/enqueue response-channel chunk))
-  Closable
-  (close [this]
-    (l/close request-channel)
-    (l/close response-channel)))
-
-(defn create-chunked-connection [request-channel ring-request]
-  (ChunkedConnection. request-channel ring-request (l/channel)))
+  {:request-channel request-channel
+   :ring-request ring-request
+   :response-channel (atom nil)
+   :state (atom :initialized)
+   :type (if (:websocket ring-request) :websocket :standard) })
 
 (defmacro defasync-route
   "Base for handling an asynchronous route."
@@ -73,44 +79,5 @@
   `(custom-handler ~path {~request-bindings :params}
      (wrap-aleph-handler
        (fn [ch# ring-request#]
-         (let [~conn-binding (~conn-fn ch# ring-request#)]
+         (let [~conn-binding (connection ch# ring-request#)]
            ~@body)))))
-
-(defprotocol RequestReplyPattern
-  "A protocol suitable for an asynchronous connection
-   that will receive a single response"
-  (respond [this response]))
-
-(defmacro defpage-async
-  "Defines an asynchronous page with a single non-streaming response.
-   Responses to the request must be answered with the 'respond' function,
-   which can be called at any point in the future
-   
-   The following async functions are available in its body: respond
-  
-  Example:
-    (defpage-async \"/foo/bar/:baz\" [baz :baz]
-      (respond {:status 200 :body \"ohai\"))"
-  [path request-bindings conn-binding & body]
-  `(defasync-route page-connection ~path ~request-bindings ~conn-binding ~@body))
-
-(defmacro defpage-chunked
-  [path request-bindings conn-binding & body]
-  `(defasync-route chunked-connection ~path ~request-bindings ~conn-binding ~@body))
-
-(defmacro defwebsocket
-  "Defines a path for use as a websocket.
-   The function body is executed 'on-open'.
-    
-   The following async functions are available in its body:
-     (on-receive (fn [msg]))
-     (on-close   (fn []))
-     (send-message \"some data\")
-  
-  Example:
-    (defwebsocket \"/path/to/websocket\"
-      (println \"Socket now open...\")
-      (on-receive (fn [msg] (send-msg \"Pong for:  \" msg)))
-      (on-close (fn [] (println \"Socket closed!\"))))"
-  [path request-bindings & body]
-  `(defasync-route websocket-connection ~path ~request-bindings ~@body))
