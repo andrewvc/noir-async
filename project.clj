f2f97afd3da04d49cfda8d5bb90a963ca90c362f
Andrew Cholakian <andrew@andrewvc.com>
2012-03-28 22:05:00 -0700

    Change version to 1.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noir-async "1.0.0-ALPHA1"
+(defproject noir-async "1.0.0-SNAPSHOT"
   :description "Async support for noir. Based on Aleph"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-beta2"]
