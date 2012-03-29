b2b2edd4ff8e724f7ee8774035323b21e780ae8f
Andrew Cholakian <andrew@andrewvc.com>
2012-03-28 20:37:26 -0700

    Removed warn-on-reflection

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject noir-async "1.0.0-ALPHA1"
   :description "Async support for noir. Based on Aleph"
-  ;;:warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-beta2"]
                  [aleph "0.2.1-SNAPSHOT"]])
