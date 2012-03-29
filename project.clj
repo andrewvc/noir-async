94a65bd414587a6a2b0429cda6909954c9348051
Andrew Cholakian <andrew@andrewvc.com>
2012-03-28 20:36:07 -0700

    Removed marginalia from project.clj

--- project.clj
+++ project.clj
@@ -2,9 +2,5 @@
   :description "Async support for noir. Based on Aleph"
   ;;:warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 ;; [noir "1.3.0-beta2" :exclusions [hiccup]]
-                 [noir "1.3.0-beta2" :exclusions [hiccup]]
-                 [aleph "0.2.1-SNAPSHOT"]]
-  ;;:dev-dependencies [[lein-marginalia "0.8.0-SNAPSHOT"]]
-  )
-
+                 [noir "1.3.0-beta2"]
+                 [aleph "0.2.1-SNAPSHOT"]])
