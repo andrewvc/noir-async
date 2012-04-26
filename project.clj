a68ef2a1ac4c2277e7db877b3073a3feaff5d4cd
Andrew Cholakian <andrew@andrewvc.com>
2012-04-25 21:07:58 -0700

    1.0.0 release!

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject noir-async "1.0.0-SNAPSHOT"
+(defproject noir-async "1.0.0"
   :description "Async support for noir. Based on Aleph"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-beta2"]
-                 [aleph "0.2.1-SNAPSHOT"]])
+                 [aleph "0.2.1-beta2"]])
