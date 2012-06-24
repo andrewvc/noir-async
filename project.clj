2eeb366552b8270ed3246661a0e07abed8c0da72
Andrew Cholakian <andrew@andrewvc.com>
2012-06-24 13:02:36 -0700

    Upgrade noir to 1.3.0-beta10

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject noir-async "1.1.0-SNAPSHOT"
   :description "Async support for noir. Based on Aleph"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir "1.3.0-beta7"]
+                 [noir "1.3.0-beta10"]
                  [aleph "0.3.0-alpha2"]])
