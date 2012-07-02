81e7f9f8bcb61c47b92aa8ef26bdf17f864df387
Andrew Cholakian <andrew@andrewvc.com>
2012-07-01 21:01:20 -0700

    Bump to beta2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noir-async "1.1.0-beta1"
+(defproject noir-async "1.1.0-beta2"
   :description "Async support for noir. Based on Aleph"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-beta10"]
