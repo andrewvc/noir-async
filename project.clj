30b712e55ca704aa18b6db78984492ff27f6a739
Andrew Cholakian <andrew@andrewvc.com>
2012-07-24 23:30:48 -0700

    Lein2 .project

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject noir-async "1.1.0-beta6"
-  :description "Async support for noir. Based on Aleph"
+(defproject noir-async/noir-async "1.1.0-beta6" 
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta10"]
-                 [aleph "0.3.0-alpha2"]])
+                 [aleph "0.3.0-alpha2"]]
+  :description "Async support for noir. Based on Aleph")
