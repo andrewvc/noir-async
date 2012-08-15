4cc4d27795c679423bfa62e6c96d0e0920ef8ad6
Andrew Cholakian <andrew@andrewvc.com>
2012-08-15 07:50:23 -0700

    Latest aleph

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject noir-async/noir-async "1.1.0-beta7" 
+(defproject noir-async/noir-async "1.1.0-beta9" 
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta10"]
-                 [aleph "0.3.0-alpha2"]]
+                 [aleph "0.3.0-SNAPSHOT"]]
   :description "Async support for noir. Based on Aleph")
